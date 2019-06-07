cf8294cf44989c19e7bd3b1ceef402695b2a8962
Kamil Debowski <xwobed@gmail.com>
2019-06-07 13:06:37 +0200

    Create lectures/binary-search directory

---
+++ lectures/binary-search/old-implementation.cpp
@@ -0,0 +1,35 @@
+// My old implementation of binary search.
+
+// 1) Looking for 'target' in a sorted array.
+int f() {
+    int left = 0, right = n - 1;
+    while(left <= right) {
+        int mid = left + (right - left) / 2;
+        if(a[mid] == target) {
+            return mid;
+        }
+        else if(a[mid] > target) {
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    return -1;
+}
+
+// 2) Looking for the index of the first element that satisfies some property.
+// F F T T T T <- find first index that has TRUE
+int f() {
+    int left = 0, right = n - 1; // add +1 to 'right' if the answer can be outside [left, right]
+    while(left < right) {
+        int mid = left + (right - left) / 2;
+        if(property(mid)) {
+            right = mid;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    return left;
+}
